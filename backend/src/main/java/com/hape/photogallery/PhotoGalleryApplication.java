9538e477e2ab1129329d42e7bf23dd0baf38f45d
HAPE <652504084@qq.com>
2026-07-03 19:27:25 +0800

    修复一系列bug，加入详细的报错信息

--- backend/src/main/java/com/hape/photogallery/PhotoGalleryApplication.java
+++ backend/src/main/java/com/hape/photogallery/PhotoGalleryApplication.java
@@ -2,10 +2,7 @@ package com.hape.photogallery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 @EnableCaching
@@ -13,13 +10,5 @@ public class PhotoGalleryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PhotoGalleryApplication.class, args);
-    }
-
-    @Bean
-    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-        FilterRegistrationBean<ShallowEtagHeaderFilter> reg = new FilterRegistrationBean<>();
-        reg.setFilter(new ShallowEtagHeaderFilter());
-        reg.addUrlPatterns("/api/*");
-        return reg;
     }
 }
